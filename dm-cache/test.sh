902edd29f3c91286fa29df55628113b656eb887d
Jesus Ramos <jesus-ramos@live.com>
2012-11-19 16:29:26 -0500

    Fixed testing script

--- dm-cache/test.sh
+++ dm-cache/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SRC="/dev/sdd1"
+SRC="/dev/sdd"
 CACHE="/dev/sdc"
 DM_CACHE="dm-cache.ko"
 
 insmod $DM_CACHE
-echo 0 167719797 cache $SRC $CACHE 0 8 1048576 1 0 node1 | dmsetup create cache
+echo 0 16777216 cache $SRC $CACHE 0 8 2097152 1 0 node1 | dmsetup create cache
 mount /dev/mapper/cache /mnt/test_mnt
