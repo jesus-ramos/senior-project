8dbe4801490b8e93d9e2d006e9b285e85af54b11
Jesus Ramos <jesus-ramos@live.com>
2012-11-18 22:48:17 -0500

    Added algorithms into presentation

--- presentation/new_cache_invalidate.c
+++ presentation/new_cache_invalidate.c
@@ -4,5 +4,6 @@ static void cache_invalidate(struct cache_c *dmc, struct cacheblock *cache)
     spin_lock(&cache->lock);
     clear_state(cache->state, VALID);
     radix_tree_delete(dmc->cache, get_block_index(cache->block,cache->disk));
+    list_del(&cache->list);
     spin_unlock(&cache->lock);
 }
