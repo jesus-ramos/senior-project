ef901e5d3b9323162adbec4accfe8a6931c1a29e
Jesus Ramos <jesus-ramos@live.com>
2012-11-27 14:28:45 -0500

    Presentation modifications

--- snippets/new_cache_hit.c
+++ snippets/new_cache_hit.c
@@ -7,5 +7,19 @@ static int cache_hit(struct cache_c *dmc, struct bio *bio,
     /* Move recently addressed block to the front of the LRU */
     list_move_tail(&cache->list, dmc->lru);
     up(&dmc->lru_mutex);
+
+#ifdef CONFIG_LFU
+    struct cacheblock *next_block;
+
+    down(&dmc->lru_mutex);
+    cache->hits++;
+    if (unlikely(list_single(dmc->lru)))
+        goto no_cmp;
+    next_block = list_entry(cache->list.prev, struct cacheblock);
+    if (cache->hits >= next_block->hits)
+        list_swap(&cache->list, &next_block->list);
+no_cmp:
+    up(&dmc->lru_mutex);
+#endif /* CONFIG_LFU */
     ...
 }
--- snippets/new_cache_miss.c
+++ snippets/new_cache_miss.c
@@ -5,8 +5,14 @@ static int cache_read_miss(struct cache_c *dmc, struct bio *bio, int disk)
     /* Read new block, insert it add the head of the LRU */
     /* Same in cache_write_miss */
     down(&dmc->lru_mutex);
+#ifndef CONFIG_LFU
     cache = list_first_entry(dmc->lru, struct cacheblock, list);
     list_move_tail(&cache->list, dmc->lru);
+#else /* CONFIG_LFU */
+    cache = list_last_entry(dmc->lru, struct cacheblock, list);
+    cache->hits = 1;
+    list_move_tail(&cache->list, dmc->lru);
     up(&dmc->lru);
+#endif /* CONFIG_LFU */
     ...
 }
